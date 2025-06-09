023fa39fcad559e39e70bfbe3f088749f1dedf20
SHuBhAM71205 <84559807+SHuBhAM71205@users.noreply.github.com>
2025-06-09 21:58:59 +0530

    Login page is ready

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,33 +1,101 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAppContext } from '../context/AppContext.jsx';
-
+import { useEffect, useState } from "react"
+import { dummyUsers as data } from "../data/DummyData";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
-  const [name, setName] = useState('');
-  const [role, setRole] = useState('user');
-  const { users, drivers, setLoggedInUser } = useAppContext();
-  const navigate = useNavigate();
+  
+  const [islogin, setisLogin] = useState(false);
+  const [user, setUser] = useState([]);
+  
+  useEffect(() => {
+    setUser(data.user);
+    console.log(data.user)
+  }, [])
+  
+  const navig = useNavigate();
 
-  const handleLogin = () => {
-    let user = null;
-    if (role === 'user') user = users.find(u => u.name === name);
-    else if (role === 'driver') user = drivers.find(d => d.name === name);
-    else user = { name, role }; // admin
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    console.log(form.password)
+    if (islogin) 
+    {
+      const usr = user.find(ele => ele.email === form.email.value)
+      console.log(usr)
+      if (usr) 
+      {
 
-    if (!user) return alert('User not found!');
-    setLoggedInUser({ ...user, role });
-    navigate(`/${role}`);
-  };
+        if (usr && usr.password == form.password.value) {
+          if (usr.role=='admin') {
+            navig('/admin')
+          }
+          else if (usr.role==='driver') {
+            navig('/driver')
+          }
+          else
+          {
+            navig('/user')
+          }
+        }
+      }
+      else 
+      {
+        console.log('Access Denied')
+      }
+    }
+    else
+    {
+       data.user.push({
+        fname:form.fname.value,
+        lname:form.lname.value,
+        email:form.email.value,
+        password:form.password.value,
+      });
 
+      setTimeout(()=>{
+        setisLogin(true);
+      },1000)
+
+    }
+
+  }  {/*pls modify this functiion as the use of the data folder*/}
   return (
-    <div>
-      <input placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
-      <select value={role} onChange={e => setRole(e.target.value)}>
-        <option value="user">User</option>
-        <option value="admin">Admin</option>
-        <option value="driver">Driver</option>
-      </select>
-      <button onClick={handleLogin}>Login</button>
+    <div className="flex items-center min-h-screen justify-center  bg-indigo-900 shadow-xl shadow-amber-300">
+      <div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-8">
+        {/* bg-cyan-500 flex-col min-h-100 rounded-md border-1 */}
+        <div>
+          <h1 className="flex items-center justify-center text-2xl">{islogin ? 'Login' : 'Signup'}</h1>
+          <div className="flex justify-center pt-6 gap-10 mb-5">
+            <button className={`flex-1 rounded-2xl h-10 ${islogin ? 'bg-indigo-400' : 'bg-indigo-100'}`} onClick={() => { setisLogin(true) }}>Login</button>
+            <button className={`flex-1 rounded-2xl h-10 ${islogin ? 'bg-indigo-100' : 'bg-indigo-400'}`} onClick={() => { setisLogin(false) }}>Signup</button>
+          </div>
+        </div>
+        <form className="flex-col space-y-7 items-center justify-center" onSubmit={handleSubmit}>
+
+          {!islogin && <> <div className=''>
+            <label htmlFor="fname" className="block pb-2">fname</label>
+
+            <input type="text" placeholder="fname" className=" px-3 py-2  w-full border rounded bg-amber-50" name="fname" required />
+          </div></>}
+
+          {!islogin && <> <div className=''>
+            <label htmlFor="lname" className="block pb-2">lname</label>
+            <input type="text" placeholder="lname" className=" px-3 py-2 block  w-full border rounded bg-amber-50" name="lname" required />
+          </div></>
+          }
+          <div className=''>
+            <label htmlFor="email" className="block pb-2">email</label>
+            <input type="text" placeholder="email" className=" px-3 py-2 block  w-full border rounded bg-amber-50 " name="email" required />
+          </div>
+
+          <div className=''>
+            <label htmlFor="password" className="block pb-2">password</label>
+            <input type="text" placeholder="password" className=" px-3 py-2 block  w-full border rounded bg-amber-50" name='password' required />
+          </div>
+
+          <button type="submit" className="mt-4 py-2 px-3 w-full border rounded hover:bg-blue-600 bg-indigo-400">{islogin ? 'Login' : 'Signup'}</button>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
+

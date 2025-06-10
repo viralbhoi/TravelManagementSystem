1cf70dbbe4487862984a62d92774fca94082714e
viralbhoi <72viralbhoi@gmail.com>
2025-06-10 13:54:47 +0530

    Fixed bug in login page

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,30 +1,49 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
-import { getFromLS, saveToLS } from '../utils/localStorageUtils';
-import { dummyUsers, dummyDrivers ,dummyAdmins} from '../data/DummyData' ;
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { getFromLS, saveToLS } from "../utils/localStorageUtils";
+import { dummyUsers, dummyDrivers, dummyAdmins } from "../data/DummyData";
 
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const [users, setUsers] = useState(getFromLS('users') || dummyUsers);
-  const [admins, setAdmins] = useState(getFromLS('admins') || dummyAdmins);
-  const [drivers, setDrivers] = useState(getFromLS('drivers') || dummyDrivers);
-  const [trips, setTrips] = useState(getFromLS('trips') || []);
-  const [loggedInUser, setLoggedInUser] = useState(getFromLS('loggedInUser') || null);
 
-  useEffect(() => {
-    saveToLS('users', users);
-    saveToLS('drivers', drivers);
-    saveToLS('trips', trips);
-    saveToLS('admins', admins)
-    saveToLS('loggedInUser', loggedInUser);
-    console.log(drivers)
-  }, [users, drivers, trips, loggedInUser,admins]);
+    const [users, setUsers] = useState(getFromLS("users") || dummyUsers);
+    const [admins, setAdmins] = useState(getFromLS("admins") || dummyAdmins);
+    const [drivers, setDrivers] = useState(
+        getFromLS("drivers") || dummyDrivers
+    );
+    const [trips, setTrips] = useState(getFromLS("trips") || []);
+    const [loggedInUser, setLoggedInUser] = useState(
+        getFromLS("loggedInUser") || null
+    );
 
-  return (
-    <AppContext.Provider value={{ admins ,users, drivers, trips, setTrips, loggedInUser, setLoggedInUser ,setAdmins,setDrivers,setUsers}}>
-      {children}
-    </AppContext.Provider>
-  );
+    
+
+    useEffect(() => {
+        saveToLS("users", users);
+        saveToLS("drivers", drivers);
+        saveToLS("trips", trips);
+        saveToLS("admins", admins);
+        saveToLS("loggedInUser", loggedInUser);
+    }, [users, drivers, trips, loggedInUser, admins]);
+
+    return (
+        <AppContext.Provider
+            value={{
+                admins,
+                users,
+                drivers,
+                trips,
+                setTrips,
+                loggedInUser,
+                setLoggedInUser,
+                setAdmins,
+                setDrivers,
+                setUsers,
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
 };
 
 export const useAppContext = () => useContext(AppContext);
--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,21 +1,57 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { useAppContext } from '../context/AppContext.jsx';
-import Login from '../pages/Login.jsx';
-import UserDashboard from '../modules/user/UserDashboard';
-import AdminDashboard from '../modules/admin/AdminDashboard.jsx';
-import DriverDashboard from '../modules/driver/DriverDashboard.jsx';
-import TripBookingForm from '../modules/user/TripBookingForm.jsx';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAppContext } from "../context/AppContext.jsx";
+import Login from "../pages/Login.jsx";
+import UserDashboard from "../modules/user/UserDashboard";
+import AdminDashboard from "../modules/admin/AdminDashboard.jsx";
+import DriverDashboard from "../modules/driver/DriverDashboard.jsx";
+import TripBookingForm from "../modules/user/TripBookingForm.jsx";
 
 export default function AppRouter() {
-  const { loggedInUser } = useAppContext();
+    const { loggedInUser } = useAppContext();
 
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/user" element={loggedInUser?.role === 'user' ? <UserDashboard /> : <Navigate to="/" />} />
-      <Route path="/admin" element={loggedInUser?.role === 'admin' ? <AdminDashboard /> : <Navigate to="/" />} />
-      <Route path="/driver" element={loggedInUser?.role === 'driver' ? <DriverDashboard /> : <Navigate to="/" />} />
-      <Route path='user-booking' element={<TripBookingForm/>}/>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route
+                path="/user"
+                element={
+                    loggedInUser?.role === "user" ? (
+                        <UserDashboard />
+                    ) : (
+                        <Navigate to="/" />
+                    )
+                }
+            />
+            <Route
+                path="/admin"
+                element={
+                    loggedInUser?.role === "admin" ? (
+                        <AdminDashboard />
+                    ) : (
+                        <Navigate to="/" />
+                    )
+                }
+            />
+            <Route
+                path="/driver"
+                element={
+                    loggedInUser?.role === "driver" ? (
+                        <DriverDashboard />
+                    ) : (
+                        <Navigate to="/" />
+                    )
+                }
+            />
+            <Route
+                path="user-booking"
+                element={
+                    loggedInUser?.role === "user" ? (
+                        <TripBookingForm />
+                    ) : (
+                        <Navigate to="/" />
+                    )
+                }
+            />
+        </Routes>
+    );
 }
